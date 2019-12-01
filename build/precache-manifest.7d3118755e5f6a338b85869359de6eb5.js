21ee88883c7e6cbc236af9d21667002ea9a213ef
mrmcgrail <tim.mcgrail77@gmail.com>
2019-11-30 21:57:08 -0500

    from my local directory

---
+++ build/precache-manifest.7d3118755e5f6a338b85869359de6eb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75cdc6d09ab5bb31032486ad20d1b849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7c872766fb7517023d00",
+    "url": "/static/css/main.effd5ef8.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "7c872766fb7517023d00",
+    "url": "/static/js/main.90648296.chunk.js"
+  },
+  {
+    "revision": "9bf820bd41e6d656f8db",
+    "url": "/static/js/runtime-main.f59df13c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
