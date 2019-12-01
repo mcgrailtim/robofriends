01024cf97d2507d66396d83d4995a38006fb08b7
mrmcgrail <tim.mcgrail77@gmail.com>
2019-11-30 22:16:08 -0500

    Updates

---
+++ precache-manifest.8cbf568a0b57fee0ce37a4e56f179f6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a522a977ef218e6c0774e0aee82c5257",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f2ccb06ec62d0295655a",
+    "url": "/robofriends/static/css/main.c2b0e1ed.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "f2ccb06ec62d0295655a",
+    "url": "/robofriends/static/js/main.e889bd4c.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
